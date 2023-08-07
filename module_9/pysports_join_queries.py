673b3d7bc8adab07c0fb0e78b22994952baf158f
coolkid558 <119823251+coolkid558@users.noreply.github.com>
2023-08-07 00:45:01 -0400

    Add files via upload

---
+++ module_9/pysports_join_queries.py
@@ -0,0 +1,39 @@
+db.student.insert({
+    1007: {
+        "Player ID": 1,
+        "First Name": 'Thorin',
+        "Last Name": 'Oakenshield',
+        "Team Name": 'Team Gandalf',
+    },
+    1008: {
+        "Player ID": 2,
+        "First Name": 'Bilbo',
+        "Last Name": 'Baggins',
+        "Team Name": 'Team Gandalf',
+    },
+    1009: {
+        "Player ID": 3,
+        "First Name": 'Frodo',
+        "Last Name": 'Baggins',
+        "Team Name": 'Team Gandalf',
+    },
+    1010: {
+        "Player ID": 4,
+        "First Name": 'Saruman',
+        "Last Name": 'The White',
+        "Team Name": 'Team Sauron',
+    },
+    1011: {
+        "Player ID": 5,
+        "First Name": 'Angmar',
+        "Last Name": 'Witch-king',
+        "Team Name": 'Team Sauron',
+    },
+    1012: {
+        "Player ID": 6,
+        "First Name": 'Azog',
+        "Last Name": 'The Defiler',
+        "Team Name": 'Team Sauron'
+    }
+})
+print(students)
